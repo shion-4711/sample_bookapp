494924abad35aefbbcec64976321afca034654f1
shion <77258886+shion-4711@users.noreply.github.com>
2022-08-07 12:11:39 +0900

    ルーティング設定

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <Header></Header>
+    <v-main>
+      <router-view/>
+    </v-main>
+    <Footer></Footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from './global/Header.vue';
+import Footer from './global/Footer.vue';
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+    name: "App",
+    data: () => ({
+    //
+    }),
+    components: { Header, Footer }
+};
+</script>
