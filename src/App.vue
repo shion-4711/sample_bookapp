4116d8772a18b1c704b289a9b7d5ca49933e4d79
shion <77258886+shion-4711@users.noreply.github.com>
2022-08-13 13:22:52 +0900

    検索機能・保存機能を追加

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <v-app>
     <Header></Header>
     <v-main>
-      <router-view/>
+      <v-container>
+        <router-view
+          :books="books"
+          @add-book-list="addBook"
+          @update-book-info="updateBookInfo"
+        />
+      </v-container>
     </v-main>
     <Footer></Footer>
   </v-app>
@@ -12,11 +18,69 @@
 import Header from './global/Header.vue';
 import Footer from './global/Footer.vue';
 
+const STORAGE_KEY = 'books';
+
 export default {
     name: "App",
-    data: () => ({
-    //
-    }),
-    components: { Header, Footer }
+    components: {
+      Header,
+      Footer,
+    },
+    data() {
+      return {
+        books: [],
+        newBook: null,
+      }
+    },
+    mounted() {
+      if (localStorage.getItem(STORAGE_KEY)) {
+        try {
+          this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        } catch(e) {
+          localStorage.removeItem(STORAGE_KEY);
+        }
+      }
+    },
+    methods: {
+      addBook(e) {
+        this.books.push({
+          id: this.books.length,
+          title: e.title,
+          image: e.image,
+          description: e.description,
+          readDate: '',
+          memo: '',
+        });
+        // this.newBook = '';
+        this.saveBooks();
+        this.goToEditPage(this.books.slice(-1)[0].id);
+      },
+      removeBook(x) {
+        this.books.slice(x,1);
+        this.saveBooks();
+      },
+      saveBooks() {
+        const parsed = JSON.stringify(this.books);
+        localStorage.setItem(STORAGE_KEY, parsed);
+      },
+      updateBookInfo(e) {
+        const updateInfo = {
+          id: e.id,
+          readDate: e.readDate,
+          memo: e.memo,
+          title: this.books[e.id].title,
+          image: this.books[e.id].image,
+          description: this.books[e.id].description
+        };
+
+        this.books.splice(e.id, 1, updateInfo);
+        this.saveBooks();
+        this.$router.push('/index');
+      },
+      goToEditPage(id) {
+        this.$router.push(`/edit/${id}`);
+      }
+    },
+    
 };
 </script>
--- src/pages/BookEdit.vue
+++ src/pages/BookEdit.vue
@@ -1,11 +1,83 @@
 <template>
   <div>
-    BookEdit
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card cols="8">
+                <v-card-title>
+                  Title: {{ book.title }}
+                </v-card-title>
+                  When you read:
+                   <v-menu
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :nudge-right="40"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="auto"
+                    >
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                          v-model="date"
+                          readonly
+                          v-bind="attrs"
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker
+                        v-model="date"
+                        @input="menu = false"
+                      ></v-date-picker>
+                    </v-menu>
+                  Notes:
+                  <v-textarea class="mx-2" v-model="book.memo">{{ book.memo }}</v-textarea>
+                <v-card-actions>
+                  <v-btn color="secondary" to="/index">To Index</v-btn>
+                  <v-btn color="info" @click="updateBookInfo">SAVE</v-btn>
+                </v-card-actions> 
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'BookEdit',
+  props: {
+    books: Array,
+  },
+  data() {
+    return {
+      book: '',
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      menu: false
+    }
+  },
+  methods: {
+    updateBookInfo() {
+      this.$emit('update-book-info', {
+        id: this.$route.params.id,
+        readDate: this.date,
+        memo: this.book.memo
+      })
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      vm.$nextTick(() => {
+        vm.book = vm.books[vm.$route.params.id]
+      })
+    })
+  },
 
 }
 </script>
